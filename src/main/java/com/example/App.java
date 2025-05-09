1697a24791e71eef8d9a5e0f6aa982e26bdb4af0
yao zhan <yao.zhan@rea-group.com>
2025-05-09 12:09:22 +0800

    invoke pipeline

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Hello world!");
+        System.out.println("Hello world!");
     }
 }
