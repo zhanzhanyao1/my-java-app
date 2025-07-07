4a0b7797341a634645fb55eb5e008c872f748a8a
yao zhan <yao.zhan@rea-group.com>
2025-07-07 14:05:57 +0800

    seperate ci, cd

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -13,6 +13,6 @@ public class App {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello world from Spring Boot-5!";
+        return "Hello world from Spring Boot-6!";
     }
 }
