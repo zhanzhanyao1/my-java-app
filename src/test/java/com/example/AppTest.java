3618595488765aa8da367e3ca6ff2cf050deaf92
yao zhan <yao.zhan@rea-group.com>
2025-07-07 13:17:04 +0800

    test&formatter

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class AppTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void helloEndpointReturnsExpectedMessage() {
+        String body = this.restTemplate.getForObject("/", String.class);
+        assertThat(body).isEqualTo("Hello world from Spring Boot-5!");
+    }
+}
