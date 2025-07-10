7550f4df5327084301bc5376b68e241cd9aca103
yao zhan <yao.zhan@rea-group.com>
2025-07-10 16:54:12 +0800

    terraform cd

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "example" {
 
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_ssh_http_prometheus"
-  description = "SSH、HTTP、Prometheus"
+  description = "SSH/HTTP/Prometheus"
 
   # 允许SSH访问（端口22），生产环境建议限制CIDR
   ingress {
