6ea14d07f2234a9f440f99dd966e2c0b7e50ab51
yao zhan <yao.zhan@rea-group.com>
2025-07-14 10:12:24 +0800

    terraform best practice

---
+++ terraform/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euxo pipefail
+echo "=== user-data script started ===" >> /var/log/user-data.log
+
+# input log
+exec > >(tee -a /var/log/user-data.log|logger -t user-data ) 2>&1
+
+echo "Updating system..."
+dnf update -y
+
+echo "Installing Docker..."
+dnf install -y docker
+
+echo "Starting Docker service..."
+systemctl start docker
+systemctl enable docker
+
+echo "Adding ec2-user to docker group..."
+usermod -aG docker ec2-user
+
+echo "=== user-data script finished ==="
